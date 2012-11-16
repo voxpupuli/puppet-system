d63172b31e7913e7dc50f0775ebd1fccb7af0e10
Keith Burdis <keith@burdis.org>
2012-11-16 14:29:55 +0000

    Populate /etc/ssh/ssh_known_hosts with SSH host keys from all Puppet nodes.

--- manifests/sshd.pp
+++ manifests/sshd.pp
@@ -1,15 +1,27 @@
 class system::sshd (
   $config         = undef,
-  $sync_host_keys = 'true'
+  $sync_host_keys = true
 ) {
   if $config {
     include augeasproviders
     $defaults = {}
     create_resources(sshd_config, $config, $defaults)
   }
-  if $sync_host_keys == 'true' {
+  if $sync_host_keys {
     # From: http://docs.puppetlabs.com/guides/exported_resources.html
-    @@sshkey { [ $::hostname, $::fqdn ]: type => rsa, key => $::sshrsakey }
+    # and https://wiki.xkyle.com/Managing_SSH_Keys_With_Puppet
+
+    # export host key
+    $hostonly = regsubst($::fqdn, "\.${::domain}$", '')
+    $host_aliases = [ $::ipaddress, $hostonly ]
+    @@sshkey { $::fqdn:
+      ensure       => present,
+      host_aliases => $host_aliases,
+      type         => 'rsa',
+      key          => $::sshrsakey,
+    }
+
+    # import all other host keys
     Sshkey <<| |>>
   }
 }
