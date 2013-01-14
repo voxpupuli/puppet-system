3ac114ead99b3768be33c19ebb03e4f3c14857d0
Keith Burdis <keith@burdis.org>
2013-01-14 11:04:32 +0000

    Changed the yumgroup type to have a default 'daily' schedule to reduce the time Puppet runs take - package group changes are usually rare after the host is first set up.  Users can override this by supplying their own schedule.

--- manifests/yumgroup.pp
+++ manifests/yumgroup.pp
@@ -2,6 +2,7 @@
 define system::yumgroup(
   $ensure   = 'present',
   $optional = false,
+  $schedule = 'daily',
   $usecache = true
 ) {
   $pkg_types_arg = $optional ? {
@@ -15,16 +16,18 @@ define system::yumgroup(
   case $ensure {
     present,installed: {
       exec { "Installing ${name} yum group":
-        command => "/usr/bin/yum -y groupinstall ${pkg_types_arg} '${name}'",
-        unless  => "/usr/bin/yum $cache grouplist 2>/dev/null | /usr/bin/perl -ne 'last if /^Available/o; next if /^\w/o; print' | /bin/grep -qw '${name}'",
-        timeout => 600,
+        command  => "/usr/bin/yum -y groupinstall ${pkg_types_arg} '${name}'",
+        unless   => "/usr/bin/yum $cache grouplist 2>/dev/null | /usr/bin/perl -ne 'last if /^Available/o; next if /^\w/o; print' | /bin/grep -qw '${name}'",
+        timeout  => 600,
+        schedule => $schedule,
       }
     }
     absent: {
       exec { "Removing ${name} yum group":
-        command => "/usr/bin/yum -y groupremove ${pkg_types_arg} '${name}'",
-        unless  => "/usr/bin/yum $cache grouplist 2>/dev/null | /usr/bin/perl -ne 'last if /^Available/o; next if /^\w/o; print' | /bin/grep -qw '${name}'",
-        timeout => 600,
+        command  => "/usr/bin/yum -y groupremove ${pkg_types_arg} '${name}'",
+        unless   => "/usr/bin/yum $cache grouplist 2>/dev/null | /usr/bin/perl -ne 'last if /^Available/o; next if /^\w/o; print' | /bin/grep -qw '${name}'",
+        timeout  => 600,
+        schedule => $schedule,
       }
     }
     default: {
