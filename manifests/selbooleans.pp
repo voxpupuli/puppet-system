1e453cf71098d37073714684cf6e599e3884d0d1
Keith Burdis <keith@burdis.org>
2013-02-10 14:58:51 +0000

    Added support for managing SELinux booleans.

---
+++ manifests/selbooleans.pp
@@ -0,0 +1,19 @@
+class system::selbooleans (
+  $config   = undef,
+  $schedule = $::system::schedule,
+) {
+  $defaults = {
+    schedule => $schedule,
+  }
+  if $config {
+    include augeasproviders
+    create_resources(selboolean, $config, $defaults)
+  }
+  else {
+    $hiera_config = hiera_hash('system::selbooleans', undef)
+    if $hiera_config {
+      include augeasproviders
+      create_resources(selboolean, $hiera_config, $defaults)
+    }
+  }
+}
