6c9a8033b7c5938b1532708fec34bfd53a871eb7
Keith Burdis <keith@burdis.org>
2013-03-11 11:08:14 +0000

    selboolean does not have an augeasprovider.

--- manifests/selbooleans.pp
+++ manifests/selbooleans.pp
@@ -7,13 +7,11 @@ class system::selbooleans (
       schedule => $schedule,
     }
     if $config {
-      include augeasproviders
       create_resources(selboolean, $config, $defaults)
     }
     else {
       $hiera_config = hiera_hash('system::selbooleans', undef)
       if $hiera_config {
-        include augeasproviders
         create_resources(selboolean, $hiera_config, $defaults)
       }
     }
