857310c0a6e8356b4d9c75f704d278e811247f69
Remko van Hunen <remko@imgzine.com>
2016-03-03 14:04:17 +0100

    Fixed typo resource to create_resource

--- manifests/ssh_authorized_keys.pp
+++ manifests/ssh_authorized_keys.pp
@@ -17,7 +17,7 @@ class system::ssh_authorized_keys (
   else {
     $hiera_config = hiera_hash('system::ssh_authorized_keys', undef)
     if $hiera_config {
-      resources(ssh_authorized_key, $hiera_config, $defaults)
+      create_resources(ssh_authorized_key, $hiera_config, $defaults)
     }
   }
 }
