87de427c3e3cd2164f37199cd2aa6966c52c9af5
Tim Meusel <tim@bastelfreak.de>
2017-11-20 11:17:00 +0100

    replace validate_* with datatypes in network.pp

--- manifests/network.pp
+++ manifests/network.pp
@@ -1,18 +1,14 @@
 class system::network (
-  $hostname     = undef,
-  $gateway      = undef,
-  $ipv6         = false,
-  $ipv6init     = false,
-  $ipv6autoconf = true,
-  $zeroconf     = false,
+  Optional[String] $hostname                    = undef,
+  Optional[Stdlib::Compat::Ip_address] $gateway = undef,
+  Boolean $ipv6                                 = false,
+  $ipv6init                                     = false,
+  $ipv6autoconf                                 = true,
+  Boolean $zeroconf                             = false,
 ) {
+
   if $hostname {
-    validate_string($hostname)
-    if ! is_ip_address($gateway) {
-      fail('system::network::gateway must be an IP address')
-    }
-    validate_bool($ipv6)
-    validate_bool($zeroconf)
+    assert_type(Stdlib::Compat::Ip_address, $gateway)
     file { '/etc/sysconfig/network':
       ensure  => present,
       owner   => 'root',
